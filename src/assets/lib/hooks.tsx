7fc69e4ef9411f6fa6d9ef820dc2c64b1a43b8f3
Snehabramhe <snehabramhe03@gmail.com>
2024-01-28 21:44:29 +0530

    type fix

--- src/assets/lib/hooks.tsx
+++ src/assets/lib/hooks.tsx
@@ -1,7 +1,7 @@
 import { useActiveSectionContext } from "../../context/active-section-context";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import type { SectionName } from "./types";
+import { SectionName } from "./types";
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const isMobile = window.innerWidth <= 1024;
